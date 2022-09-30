0f63780e00de57e6d75dc9b366af7fceb042473e
fabianrodrigues <80415510+fabianrodrigues@users.noreply.github.com>
2022-09-30 09:34:33 +0100

    Add files via upload

---
+++ GetAppInventory.cmd
@@ -0,0 +1,2 @@
+"%~dp0psexec.exe" -accepteula @"%~dp0computerlist.txt" -h -u "fabian.rodrigues@test-and-trace.nhs.uk" powershell.exe "C:\Users\fabian.rodrigues\OneDrive - NIHP Test and Trace\Documents\WVD PS\WVD PS keep\App Inventory\Get-Software.ps1"
+pause
---
+++ InvokeCommand-GetSoftware.ps1
@@ -0,0 +1,16 @@
+﻿
+
+#  C:\temp\fslogix\x64\Release\FSLogixAppsSetup.exe
+
+#$listofcomputernaems = "ZNEEVCSP16VC512"
+$listofcomputernaems = get-content -Path $PSScriptRoot\computerlist.txt
+
+
+foreach ($c in $listofcomputernaems) {
+  write-host $c
+  Invoke-Command -ComputerName $c -ScriptBlock {
+   start-process -FilePath "C:\Temp\AppInventory\GetAppInventory.cmd" -wait 
+   #dir c:\
+ }
+}
+
